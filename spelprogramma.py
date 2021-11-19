98ecf85064fb5bb145507812bdd7b414c14d5043
maxhofmanf <99068281@mydavinci.nl>
2021-11-19 11:28:14 +0100

    collections

--- spelprogramma.py
+++ spelprogramma.py
@@ -1,5 +1,9 @@
 import random
-spel = ['monopoly','yathzee','bridge','poker','pesten','mens erger je niet','cluedo']
-num =random.randrange(3, 10)
-for x in range (num):
-    print(random.choice(spel))
+spellist = ['monopoly','yathzee','bridge','poker','pesten','mens erger je niet','cluedo']
+
+def spelProgramma(spellist,minimum, maximum):
+    num =random.randrange(minimum, maximum)
+    for x in range (num):
+        print(random.choice(spellist))
+
+spelProgramma(spellist,3,10)
