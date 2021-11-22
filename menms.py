d816ca58d7199980d6308ed8d6635c4a61a1a77e
maxhofmanf <99068281@mydavinci.nl>
2021-11-22 09:55:03 +0100

    collections

--- menms.py
+++ menms.py
@@ -1,11 +1,14 @@
 import random 
 mm = ["oranje","bruin","blauw","bruin"]
-
 aantal = int(input("hoeveel m&m's wilt u? "))
+b = []
 
 def randommenm(aantal):
+    
     for x in range (aantal):
         zakm = random.choice(mm)
-        print(zakm)
+        b.append(zakm)
+    print (b)    
+
 
 randommenm(aantal)
