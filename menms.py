ea2d4f23cf0522a963203ff424af8c0903b960d5
maxhofmanf <99068281@mydavinci.nl>
2021-11-26 09:21:50 +0100

    collections

--- menms.py
+++ menms.py
@@ -1,14 +1,35 @@
 import random 
-mm = ["oranje","bruin","blauw","bruin"]
+mm = ["oranje","bruin","blauw","groen"]
 aantal = int(input("hoeveel m&m's wilt u? "))
-b = []
+damm={
 
-def randommenm(aantal):
-    
-    for x in range (aantal):
+    "groen":0,
+    "blauw":0,
+    "oranje":0,
+    "bruin":0
+}
+color1 = 0
+color2 = 0
+color3 = 0
+color4 = 0
+
+def aantalmm():
+    damm["groen"] = color1
+    damm["blauw"] = color2
+    damm["oranje"] = color3
+    damm["bruin"] = color4
+
+def randommenm():
+    global zakm, color1,color2,color3,color4
+    for x in range(aantal):
         zakm = random.choice(mm)
-        b.append(zakm)
-    print (*b)    
+        mm.append(zakm)
+        color1 += zakm.count("groen")
+        color2 += zakm.count("blauw")
+        color3 += zakm.count("oranje")
+        color4 += zakm.count("bruin")
 
 
-randommenm(aantal)
+randommenm()
+aantalmm()
+print(damm)
